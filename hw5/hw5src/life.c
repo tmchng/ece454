3fbf358e8c6b27a8984c85756ea56eead5b5aa1d
Tim Cheng <timcheng1006@gmail.com>
2014-12-02 20:20:49 -0500

    removed global variables

--- hw5/hw5src/life.c
+++ hw5/hw5src/life.c
@@ -34,70 +34,96 @@ struct board_section {
   int j_size;
 };
 
-// Global board variables
-char *outboard;
-char *inboard;
-unsigned board_size;
-unsigned curgen = 0;
-unsigned gens_max;
+struct pkg {
+  int t_i;
+  char *inboard;
+  char *outboard;
+  int board_size;
+  int gens_max;
+  pthread_mutex_t *runner_wait_mutex;
+  pthread_cond_t *runner_wait_cv;
+  pthread_mutex_t *runner_start_mutex;
+  pthread_cond_t *runner_start_cv;
+  int *waiting_runners;
+  int *runner_start;
+};
 
-pthread_t threads[NUM_THREADS];
-int waiting_runners = 0; // Number of runners waiting
-pthread_mutex_t runner_wait_mutex;
-pthread_cond_t runner_wait_cv;
-int runner_start[NUM_THREADS] = {0}; // Indicates if a runner should start
-pthread_mutex_t runner_start_mutexes[NUM_THREADS];
-pthread_cond_t runner_start_cvs[NUM_THREADS];
 
-
-void multithread_init();
-void resume_task_runners();
-void get_board_section(struct board_section *section, int t_i);
+void resume_task_runners(struct pkg pkgs[]);
+void get_board_section(struct board_section *section, int board_size, int t_i);
 void *gol_task_runner(void *args);
 int get_thread_index();
+
 
 /*****************************************************************************
  * Game of life implementation
  ****************************************************************************/
 char*
-game_of_life (char* _outboard,
-	      char* _inboard,
+game_of_life (char* outboard,
+	      char* inboard,
 	      const int nrows,
 	      const int ncols,
-	      const int _gens_max)
+	      const int gens_max)
 {
-  assert(_outboard && _inboard);
+  assert(outboard && inboard);
   assert(nrows == ncols);
-  board_size = nrows;
+  const int board_size = nrows;
   assert(board_size >= MIN_BOARD_SIZE);
   assert((board_size & (board_size-1)) == 0);
 
-  if (_gens_max <= 0) return _inboard;
+  if (gens_max <= 0) return inboard;
 
   if (board_size <= 32) {
     // No need to parallelize
     return sequential_game_of_life (outboard, inboard, nrows, ncols, gens_max);
   }
 
-  // Parallelize
+  // Parallelize starting here.
   int i, retval;
 
-  inboard = _inboard;
-  outboard = _outboard;
-  gens_max = _gens_max;
-  curgen = 0;
+  // Declaring everything needed to synchronize threads
+  pthread_t threads[NUM_THREADS];
+  int waiting_runners = 0; // Number of runners waiting
+  int runner_start[NUM_THREADS] = {0}; // Indicates if a runner should start
+  pthread_mutex_t runner_wait_mutex;
+  pthread_cond_t runner_wait_cv;
+  pthread_mutex_t runner_start_mutexes[NUM_THREADS];
+  pthread_cond_t runner_start_cvs[NUM_THREADS];
 
-  multithread_init();
+  // Initialize mutexes and cvs.
+  pthread_mutex_init(&runner_wait_mutex, NULL);
+  pthread_cond_init(&runner_wait_cv, NULL);
+  for (i = 0; i < NUM_THREADS; i++){
+    pthread_mutex_init(&runner_start_mutexes[i], NULL);
+    pthread_cond_init(&runner_start_cvs[i], NULL);
+  }
+
+  // construct packages because global variables are not allowed.
+  struct pkg pkgs[NUM_THREADS];
+  for (i = 0; i < NUM_THREADS; i++) {
+    pkgs[i].t_i = i;
+    pkgs[i].inboard = inboard;
+    pkgs[i].outboard = outboard;
+    pkgs[i].board_size = board_size;
+    pkgs[i].gens_max = gens_max;
+    pkgs[i].runner_wait_mutex = &runner_wait_mutex;
+    pkgs[i].runner_wait_cv = &runner_wait_cv;
+    pkgs[i].runner_start_mutex = &runner_start_mutexes[i];
+    pkgs[i].runner_start_cv = &runner_start_cvs[i];
+    pkgs[i].waiting_runners = &waiting_runners;
+    pkgs[i].runner_start = &runner_start[i];
+  }
 
   // Start task runners
   for (i = 0; i < NUM_THREADS; i++) {
-    retval = pthread_create(&threads[i], NULL, &gol_task_runner, NULL);
+    retval = pthread_create(&threads[i], NULL, &gol_task_runner, &pkgs[i]);
     if (retval) {
       printf("\n*** Error: pthread_create() return code: %d\n", retval);
       exit(EXIT_FAILURE);
     }
   }
 
+  int curgen = 0;
   while (curgen < gens_max) {
     //printf("wait curgen\n");
     // Wait for threads to finish the current gen
@@ -108,10 +134,9 @@ game_of_life (char* _outboard,
     //printf("exit from wait curgen\n");
     curgen++;
     waiting_runners = 0;
-    SWAP_BOARDS(outboard, inboard);
     pthread_mutex_unlock(&runner_wait_mutex);
 
-    resume_task_runners();
+    resume_task_runners(pkgs);
   }
 
   // Wait for runners to finish
@@ -122,18 +147,18 @@ game_of_life (char* _outboard,
   return inboard;
 }
 
-void resume_task_runners() {
+void resume_task_runners(struct pkg pkgs[]) {
   int i;
   for (i = 0; i < NUM_THREADS; i++) {
     //printf("resume task runner %d\n", i);
-    pthread_mutex_lock(&runner_start_mutexes[i]);
-    runner_start[i] = 1;
-    pthread_cond_signal(&runner_start_cvs[i]);
-    pthread_mutex_unlock(&runner_start_mutexes[i]);
+    pthread_mutex_lock(pkgs[i].runner_start_mutex);
+    *(pkgs[i].runner_start) = 1;
+    pthread_cond_signal(pkgs[i].runner_start_cv);
+    pthread_mutex_unlock(pkgs[i].runner_start_mutex);
   }
 }
 
-void get_board_section(struct board_section *section, int t_i) {
+void get_board_section(struct board_section *section, int board_size, int t_i) {
   assert(t_i >= 0);
   assert(section);
 
@@ -150,9 +175,22 @@ void get_board_section(struct board_section *section, int t_i) {
   section->j_size = j_size;
 }
 
-void *gol_task_runner(void *args) {
-  const int t_i = get_thread_index();
+void gol_task_runner_2(void *args) {
+  // Tiled version
+  // Unpack pkg
+  const struct pkg *pkg = (struct pkg*) args;
+  const int board_size = pkg->board_size;
   const int LDA = board_size;
+  const int t_i = pkg->t_i;
+  char *inboard = pkg->inboard;
+  char *outboard = pkg->outboard;
+  const int gens_max = pkg->gens_max;
+  pthread_mutex_t *runner_wait_mutex = pkg->runner_wait_mutex;
+  pthread_cond_t *runner_wait_cv = pkg->runner_wait_cv;
+  pthread_mutex_t *runner_start_mutex = pkg->runner_start_mutex;
+  pthread_cond_t *runner_start_cv = pkg->runner_start_cv;
+  int *waiting_runners = pkg->waiting_runners;
+  int *runner_start = pkg->runner_start;
 
   int start_i, start_j, i_size, j_size;
   int i, j, i2, j2;
@@ -160,7 +198,7 @@ void *gol_task_runner(void *args) {
   char neighbor_count;
   struct board_section section;
 
-  get_board_section(&section, t_i);
+  get_board_section(&section, board_size, t_i);
   start_i = section.start_i;
   start_j = section.start_j;
   j_size = section.j_size;
@@ -172,6 +210,7 @@ void *gol_task_runner(void *args) {
   int j2_end = start_j + j_size;
   int i2_end = start_i + i_size;
 
+  int curgen = 0;
 
   while (curgen < gens_max) {
     // TODO: tile the loop
@@ -208,47 +247,60 @@ void *gol_task_runner(void *args) {
     }
 
     // Signal that the runner is done and waiting
-    pthread_mutex_lock(&runner_wait_mutex);
-    //printf("runner %d done\n", t_i);
-    waiting_runners++;
-    if (waiting_runners >= NUM_THREADS) {
+    pthread_mutex_lock(runner_wait_mutex);
+    *waiting_runners = *waiting_runners + 1;
+    //printf("runner %d done: %dth\n", t_i, *waiting_runners);
+    if (*waiting_runners >= NUM_THREADS) {
       //printf("runner %d signal wait cv\n", t_i);
-      pthread_cond_signal(&runner_wait_cv);
+      pthread_cond_signal(runner_wait_cv);
     }
-    pthread_mutex_unlock(&runner_wait_mutex);
+    pthread_mutex_unlock(runner_wait_mutex);
 
     // Wait for start signal before running next gen
-    pthread_mutex_lock(&runner_start_mutexes[t_i]);
+    pthread_mutex_lock(runner_start_mutex);
     //printf("runner %d wait for start signal\n", t_i);
-    while (runner_start[t_i] == 0) {
-      pthread_cond_wait(&runner_start_cvs[t_i], &runner_start_mutexes[t_i]);
+    while (*runner_start == 0) {
+      pthread_cond_wait(runner_start_cv, runner_start_mutex);
     }
     //printf("runner %d resuming\n", t_i);
-    runner_start[t_i] = 0;
-    pthread_mutex_unlock(&runner_start_mutexes[t_i]);
-  }
+    *runner_start = 0;
+    pthread_mutex_unlock(runner_start_mutex);
 
-  pthread_exit(NULL);
+    SWAP_BOARDS(outboard, inboard);
+    curgen++;
+  }
 }
 
-/*
-void *gol_task_runner(void *args) {
-  const int t_i = get_thread_index();
+void gol_task_runner_1(void *args) {
+  // Unpack pkg
+  const struct pkg *pkg = (struct pkg*) args;
+  const int board_size = pkg->board_size;
   const int LDA = board_size;
+  const int t_i = pkg->t_i;
+  char *inboard = pkg->inboard;
+  char *outboard = pkg->outboard;
+  const int gens_max = pkg->gens_max;
+  pthread_mutex_t *runner_wait_mutex = pkg->runner_wait_mutex;
+  pthread_cond_t *runner_wait_cv = pkg->runner_wait_cv;
+  pthread_mutex_t *runner_start_mutex = pkg->runner_start_mutex;
+  pthread_cond_t *runner_start_cv = pkg->runner_start_cv;
+  int *waiting_runners = pkg->waiting_runners;
+  int *runner_start = pkg->runner_start;
 
   int start_i, start_j, i_size, j_size;
   int i, j;
   int inorth, isouth, jwest, jeast;
   char neighbor_count;
   struct board_section section;
+  int curgen = 0;
 
-  get_board_section(&section, t_i);
+  get_board_section(&section, board_size, t_i);
   start_i = section.start_i;
   start_j = section.start_j;
   j_size = section.j_size;
   i_size = section.i_size;
 
-  printf("runner %d gets %d to %d\n", t_i, start_j, start_j + j_size - 1);
+  //printf("runner %d gets %d to %d\n", t_i, start_j, start_j + j_size - 1);
 
   while (curgen < gens_max) {
     // TODO: tile the loop
@@ -275,45 +327,34 @@ void *gol_task_runner(void *args) {
     }
 
     // Signal that the runner is done and waiting
-    pthread_mutex_lock(&runner_wait_mutex);
-    //printf("runner %d done\n", t_i);
-    waiting_runners++;
-    if (waiting_runners >= NUM_THREADS) {
+    pthread_mutex_lock(runner_wait_mutex);
+    *waiting_runners = *waiting_runners + 1;
+    //printf("runner %d done: %dth\n", t_i, *waiting_runners);
+    if (*waiting_runners >= NUM_THREADS) {
       //printf("runner %d signal wait cv\n", t_i);
-      pthread_cond_signal(&runner_wait_cv);
+      pthread_cond_signal(runner_wait_cv);
     }
-    pthread_mutex_unlock(&runner_wait_mutex);
+    pthread_mutex_unlock(runner_wait_mutex);
 
     // Wait for start signal before running next gen
-    pthread_mutex_lock(&runner_start_mutexes[t_i]);
+    pthread_mutex_lock(runner_start_mutex);
     //printf("runner %d wait for start signal\n", t_i);
-    while (runner_start[t_i] == 0) {
-      pthread_cond_wait(&runner_start_cvs[t_i], &runner_start_mutexes[t_i]);
+    while (*runner_start == 0) {
+      pthread_cond_wait(runner_start_cv, runner_start_mutex);
     }
     //printf("runner %d resuming\n", t_i);
-    runner_start[t_i] = 0;
-    pthread_mutex_unlock(&runner_start_mutexes[t_i]);
+    *runner_start = 0;
+    pthread_mutex_unlock(runner_start_mutex);
+
+    SWAP_BOARDS(outboard, inboard);
+    curgen++;
   }
 
+}
+
+void *gol_task_runner(void *args) {
+  //gol_task_runner_1(args);
+  gol_task_runner_2(args);
   pthread_exit(NULL);
 }
-*/
 
-void multithread_init() {
-  int i;
-  pthread_mutex_init(&runner_wait_mutex, NULL);
-  pthread_cond_init(&runner_wait_cv, NULL);
-  for (i = 0; i < NUM_THREADS; i++){
-    pthread_mutex_init(&runner_start_mutexes[i], NULL);
-    pthread_cond_init(&runner_start_cvs[i], NULL);
-  }
-}
-
-int get_thread_index() {
-  int i;
-  pthread_t self = pthread_self();
-  for (i = 0; i < NUM_THREADS; i++) {
-    if (pthread_equal(self, threads[i])) return i;
-  }
-  return -1;
-}
